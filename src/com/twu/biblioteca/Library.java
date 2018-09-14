001f09213a1eb3f643910bfb34e6ccaf13476677
jo.lenis@thoughtworks.com <Q1w1e1r1>
2018-09-14 13:01:17 +0200

    List books & print list books

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -14,12 +14,19 @@ class Library {
         return bookList;
     }
 
-     void setBookList(List<Book> bookList) {
+    void setBookList(List<Book> bookList) {
         this.bookList = bookList;
     }
 
+    void printAvailableBooks() {
+        System.out.printf("%-10s %-10s %-10s %-10s%n", "Index", "Title", "Author", "Year");
+        for (Book b : this.bookList) {
+            if (b.isAvailability()) {
+                System.out.printf("%-10.10s %-10.10s %-10.10s %-10.10s%n", this.bookList.indexOf(b), b.getTitle(), b.getAuthor(), String.valueOf(b.getYear()));
 
-
+            }
+        }
+    }
 
 
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,9 +1,13 @@
 package com.twu.biblioteca;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +15,18 @@ public class LibraryTest {
     private Book b1 = new Book("To Kill a Mockingbird", "Harper Lee", 1964);
     private List<Book> emptyList = new ArrayList<Book>();
     private Library bangaloreLibrary = new Library(emptyList);
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {System.setOut(originalOut);
+    }
+
 
     @Test
     public void emptyBookListTest() {
@@ -20,10 +36,16 @@ public class LibraryTest {
     }
 
     @Test
-    public void BookListTest() {
+    public void bookListTest() {
         bangaloreLibrary.getBookList().add(b1);
-
         assertEquals(1, bangaloreLibrary.getBookList().size());
 
+    }
+
+    @Test
+    public void printAvailableBooksTest() {
+        bangaloreLibrary.getBookList().add(b1);
+        bangaloreLibrary.printAvailableBooks();
+        assertEquals("Index      Title      Author     Year      \n0          To Kill a  Harper Lee 1964      \n", outContent.toString());
     }
 }
